c6dd646c9f438251abb5922bec02927b93dc17ea
Rahul Raj <raj.rahul686@gmail.com>
2025-12-20 16:12:29 +0530

    added handlers for adding and removing items

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -4,28 +4,52 @@ import Header from "../ShoppingHeader/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./ShoppingList.css";
-
-const shoppingItems = [
-  {
-    id: 1,
-    name: "Apple",
-    quantity: 9,
-  },
-  {
-    id: 1,
-    name: "Grapes",
-    quantity: 19,
-  },
-];
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function ShoppingList() {
+  const [shoppingItems, setShoppingItems] = useState([]);
+
+  function handleAddItem(itemName) {
+    setShoppingItems([
+      ...shoppingItems,
+      {
+        id: uuidv4(),
+        name: itemName,
+        quantity: 1,
+      },
+    ]);
+  }
+
+  function handleAddQuantity(itemId) {
+    setShoppingItems(
+      shoppingItems.map((item) => {
+        if (item.id === itemId) item.quantity++;
+        return item;
+      })
+    );
+  }
+
+  function handleDecreaseQuantity(itemId) {
+    let newShoppingList = shoppingItems.map((item) => {
+      if (item?.id === itemId) item.quantity--;
+      return item;
+    });
+    newShoppingList = newShoppingList.filter((item) => item.quantity > 0);
+    setShoppingItems(newShoppingList);
+  }
+
   return (
     <div>
       <Header />
       <ToastContainer />
       <div className="current-shopping-list">
-        <Inputitem />
-        <ItemList shoppingItems={shoppingItems} />
+        <Inputitem addItem={handleAddItem} />
+        <ItemList
+          shoppingItems={shoppingItems}
+          addQunatity={handleAddQuantity}
+          decQuantity={handleDecreaseQuantity}
+        />
       </div>
     </div>
   );
