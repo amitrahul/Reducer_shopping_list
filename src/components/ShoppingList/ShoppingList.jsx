8290b984d55fe81de49ed6485bbe5998fb15661f
Rahul Raj <raj.rahul686@gmail.com>
2025-12-20 18:54:46 +0530

    refactor it and use reducer logic

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,15 +1,39 @@
+import { useReducer, useState } from "react";
 import Inputitem from "../InputItem/InputItem";
 import ItemList from "../ItemList/ItemList";
 import Header from "../ShoppingHeader/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./ShoppingList.css";
-import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import itemReducer from "../../reducers/itemReducer";
 
 function ShoppingList() {
-  const [shoppingItems, setShoppingItems] = useState([]);
+  const [shoppingItems, dispatch] = useReducer(itemReducer, []);
 
+  function handleAddItem(itemName) {
+    dispatch({
+      type: "ADD_ITEM",
+      payload: itemName,
+    });
+  }
+
+  function handleAddQuantity(itemId) {
+    dispatch({
+      type: "INCREMENT_ITEM",
+      payload: itemId,
+    });
+  }
+
+  function handleDecreaseQuantity(itemId) {
+    dispatch({
+      type: "DECREMENT_ITEM",
+      payload: itemId,
+    });
+  }
+
+  /*
+  const [shoppingItems, setShoppingItems] = useState([]);
   function handleAddItem(itemName) {
     setShoppingItems([
       ...shoppingItems,
@@ -38,6 +62,7 @@ function ShoppingList() {
     newShoppingList = newShoppingList.filter((item) => item.quantity > 0);
     setShoppingItems(newShoppingList);
   }
+    */
 
   return (
     <div>
